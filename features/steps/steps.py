c5338d29d89083988836baea7832c235f0b631d6
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2019-07-18 16:13:02 +0200

    Partial fix for encryption with mirror
    I've logged an issue against this as there is a regression trying to
    search for replicated packages in the storage service. This feature
    looks like it might work once that issue is fixed.
    
        sudo make test-at-behave TAGS=aip-encrypt-mirror BROWSER=Firefox

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -344,14 +344,14 @@ def step_impl(context):
     """
     context.execute_steps(
         'When the user waits for the "Assign UUIDs to directories?" decision point to appear and chooses "No" during transfer\n'
-        'And the user waits for the "Select file format identification command" decision point to appear and chooses "Identify using Siegfried" during transfer\n'
+        'And the user waits for the "Do you want to perform file format identification?" decision point to appear and chooses "Yes" during transfer\n'
         'And the user waits for the "Perform policy checks on originals?" decision point to appear and chooses "No" during transfer\n'
         'And the user waits for the "Create SIP(s)" decision point to appear and chooses "Create single SIP and continue processing" during transfer\n'
         'And the user waits for the "Normalize" decision point to appear and chooses "Normalize for preservation" during ingest\n'
         'And the user waits for the "Approve normalization (review)" decision point to appear and chooses "Approve" during ingest\n'
         'And the user waits for the "Perform policy checks on preservation derivatives?" decision point to appear and chooses "No" during ingest\n'
         'And the user waits for the "Perform policy checks on access derivatives?" decision point to appear and chooses "No" during ingest\n'
-        'And the user waits for the "Select file format identification command|Process submission documentation" decision point to appear and chooses "Identify using Siegfried" during ingest\n'
+        'And the user waits for the "Do you want to perform file format identification?|Process submission documentation" decision point to appear and chooses "Yes" during ingest\n'
         'And the user waits for the "Bind PIDs?" decision point to appear and chooses "No" during ingest\n'
         'And the user waits for the "Document empty directories?" decision point to appear and chooses "No" during ingest\n'
         'And the user waits for the "Store AIP (review)" decision point to appear and chooses "Store AIP" during ingest'
